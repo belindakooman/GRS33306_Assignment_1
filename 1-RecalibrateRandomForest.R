cade75268b2f91eba2b9d6098f107cfef30debc4
belindakooman <76947400+belindakooman@users.noreply.github.com>
2021-02-18 14:21:08 +0100

    Completed import code. Working on model fitting.

--- 1-RecalibrateRandomForest.R
+++ 1-RecalibrateRandomForest.R
@@ -35,8 +35,23 @@ rl_sin_asp <- sin(rl_aspect)
 rl_cos_asp <- cos(rl_aspect)
 
 # Combine all other layers into a multiraster stack.
-multiraster <- stack(c(list_of_rasters[2:14], rl_sin_asp, rl_cos_asp))
+rs_covariates <- stack(c(list_of_rasters[2:14], rl_sin_asp, rl_cos_asp))
 # Update names of the sine and cosine layers.
-names(multiraster)[14:15] <- c("sin_asp", "cos_asp")
+names(rs_covariates)[14:15] <- c("sin_asp", "cos_asp")
 # view the results.
-plot(multiraster)
+plot(rs_covariates)
+
+# Replace unwanted landuse categories with NA.
+rs_covariates$landuse <- reclassify(rs_covariates$landuse, rcl = matrix(c(12, NA), nrow = 1))
+
+# Read only the relevant attributes of the soil observations point data (X and Y coordinates plus true SOC contents).
+df_SOC <- read.csv(file = "SOC_top_soil.csv", header = TRUE)[,c("X", "Y", "SOC")]
+
+# ---------------------------
+# 3. Performing model fitting
+# ---------------------------
+
+# Set a seed.
+set.seed(150)
+# Split the points into calibration and validation sets.
+vn_rownumbers_validation <- sample(1:nrow(df_SOC), size = 250)
