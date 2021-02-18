052945a7107478cd54e527ca681c256e83c7d4e0
ynekevaniersel <75794716+ynekevaniersel@users.noreply.github.com>
2021-02-18 16:12:39 +0100

    finalize the preparation of the soil worldmap
    finalize the preparation of the soil worldmap

--- 2-prepare_worldmap_SOC.R
+++ 2-prepare_worldmap_SOC.R
@@ -21,17 +21,114 @@ if( !("corrplot" %in% installed.packages()[,1]) )
 library("corrplot")
 
 #### Prepare and merge the raster files #####
+
 # Make sure to correctly set the working directory
 ## method 1: ajust and run the lines below (adjust to your prefered directory)
 data_dir <- paste("C:/Users/lenovoa/Desktop/GRS/Week1/Pratical/Day3/data_day3/data/spatial-covariates")
 setwd(data_dir)
 ## method 2: go to 'sesion' -> 'set working directory' -> 'to source file location'
 
-## load the raster layers
+
+# Load the raster layers
 rl_soc_0_5_nr1 <- raster("1_worldmap_SOC_0_5cm.tif")
 rl_soc_0_5_nr2 <- raster("2_worldmap_SOC_0_5cm.tif")
 rl_soc_0_5_nr3 <- raster("3_worldmap_SOC_0_5cm.tif")
+rl_soc_0_5_nr4 <- raster("4_worldmap_SOC_0_5cm.tif")
+rl_soc_0_5_nr5 <- raster("5_worldmap_SOC_0_5cm.tif")
+rl_soc_0_5_nr6 <- raster("6_worldmap_SOC_0_5cm.tif")
+
+rl_soc_5_15_nr1 <- raster("1_worldmap_SOC_5_15cm.tif")
+rl_soc_5_15_nr2 <- raster("2_worldmap_SOC_5_15cm.tif")
+rl_soc_5_15_nr3 <- raster("3_worldmap_SOC_5_15cm.tif")
+rl_soc_5_15_nr4 <- raster("4_worldmap_SOC_5_15cm.tif")
+rl_soc_5_15_nr5 <- raster("5_worldmap_SOC_5_15cm.tif")
+rl_soc_5_15_nr6 <- raster("6_worldmap_SOC_5_15cm.tif")
+
+rl_soc_15_30_nr1 <- raster("1_worldmap_SOC_15_30cm.tif")
+rl_soc_15_30_nr2 <- raster("2_worldmap_SOC_15_30cm.tif")
+rl_soc_15_30_nr3 <- raster("3_worldmap_SOC_15_30cm.tif")
+rl_soc_15_30_nr4 <- raster("4_worldmap_SOC_15_30cm.tif")
+rl_soc_15_30_nr5 <- raster("5_worldmap_SOC_15_30cm.tif")
+rl_soc_15_30_nr6 <- raster("6_worldmap_SOC_15_30cm.tif")
+
+
+
+## merge the raster layers per depth (so 1 rasterlayer for 0-5 cm, 5-15 and 15-30 cm)
+# added the tolerance to enable to merge the layers without a big overlap
+rl_soc_0_5cm <- raster::merge(rl_soc_0_5_nr1, 
+                              rl_soc_0_5_nr2, 
+                              rl_soc_0_5_nr3,
+                              rl_soc_0_5_nr4,
+                              rl_soc_0_5_nr5,
+                              rl_soc_0_5_nr6,
+                              tolerance=0.5)
+
+rl_soc_5_15cm <- raster::merge(rl_soc_5_15_nr1, 
+                              rl_soc_5_15_nr2, 
+                              rl_soc_5_15_nr3,
+                              rl_soc_5_15_nr4,
+                              rl_soc_5_15_nr5,
+                              rl_soc_5_15_nr6,
+                              tolerance=0.5)
+
+
+rl_soc_15_30cm <- raster::merge(rl_soc_15_30_nr1, 
+                               rl_soc_15_30_nr2, 
+                               rl_soc_15_30_nr3,
+                               rl_soc_15_30_nr4,
+                               rl_soc_15_30_nr5,
+                               rl_soc_15_30_nr6,
+                               tolerance=0.5)
 
 
 
 
+# check and plot the layers
+plot(rl_soc_0_5cm)
+plot(rl_soc_5_15cm)
+plot(rl_soc_15_30cm)
+
+# Clean working directory
+remove(rl_soc_0_5_nr1, 
+       rl_soc_0_5_nr2, 
+       rl_soc_0_5_nr3,
+       rl_soc_0_5_nr4,
+       rl_soc_0_5_nr5,
+       rl_soc_0_5_nr6,
+       rl_soc_5_15_nr1, 
+       rl_soc_5_15_nr2, 
+       rl_soc_5_15_nr3,
+       rl_soc_5_15_nr4,
+       rl_soc_5_15_nr5,
+       rl_soc_5_15_nr6,
+       rl_soc_15_30_nr1, 
+       rl_soc_15_30_nr2, 
+       rl_soc_15_30_nr3,
+       rl_soc_15_30_nr4,
+       rl_soc_15_30_nr5,
+       rl_soc_15_30_nr6)
+
+
+#### Merge the depth layers and export ####
+# adds up the SOC data from every layer
+rl_SOC_total_weighted = ((rl_soc_0_5cm*5) + (rl_soc_5_15cm*10) + (rl_soc_15_30cm*15)/30)
+
+
+# download the boundaries of NL
+sp_df_NL <- getData(name    = 'GADM', 
+                          country = 'NLD', 
+                          level   = 0)
+
+
+## Create a cropped raster layer based on the boundaries of the NL
+rl_SOC_total_weighted_crop <- crop(x = rl_SOC_total_weighted, y = sp_df_NL)
+
+# check differnces
+plot(rl_SOC_total_weighted)
+plot(rl_SOC_total_weighted_crop)
+
+
+
+
+# Export the SOC map of source (worldmap soil grid)
+writeRaster(rl_SOC_total_weighted_crop, 'SOC_worldmap.tif', overwrite=TRUE)
